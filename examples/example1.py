c126c757a34f0cb299d76ce768b9f6a7777ef09c
Julien Palard <julien@palard.fr>
2018-04-07 22:56:20 +0200

    Refreshing Python example.

--- examples/example1.py
+++ examples/example1.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import pprint
 import sys
-sys.path.append('..')
-
 from logtop import logtop
 
 
 """
-
 This example show a complete usage of the python API,
-It can be used like this :
+It can be used like this:
 
 $ make python-module
-$ cat /etc/passwd | cut -d: -f7 | python example1.py
+$ python3 -m venv logtop_venv
+$ . logtop_venv/bin/activate
+$ python3 -m pip install .
+$
+$ cat /etc/passwd | cut -d: -f7 | python3 examples/example1.py
 
 """
 
